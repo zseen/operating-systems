47eb9cb05d96517cfc9bb923584f585d9d49ea1f
zseen <43151120+zseen@users.noreply.github.com>
2018-10-10 21:04:10 +0100

    Add method openFile() and cat():
    - openFile() opens the requested .txt file in the directory
    - cat() puts two .txt files together in a third file

--- ListFolders.py
+++ ListFolders.py
@@ -6,11 +6,16 @@ class Actions:
         self.currentDirectory = "C:\\Users"
         print(self.currentDirectory)
 
-    def ls(self):
+    def ls(self, instruction):
         print("")
         print("The content of " + self.currentDirectory + " is: ")
-        for item in os.listdir(self.currentDirectory):
-            print(item)
+
+        if len(instruction) == 1 or instruction[1] == "/W":
+            print(', '.join(os.listdir(self.currentDirectory)))
+
+        elif instruction[1] == "/D":
+            for item in os.listdir(self.currentDirectory):
+                print(item)
         print("")
 
     def cd(self, instruction):
@@ -22,11 +27,25 @@ class Actions:
             print("You are in " + self.currentDirectory + " now")
         else:
             self.currentDirectory = directoryBeforeChange
-            print("No such directory, I will return to " + self.currentDirectory )
-            
+            print("No such directory to step into, I will return to " + self.currentDirectory)
 
+    def cat(self, file1, file2):
+        with open("c.txt", "w") as catFile:
+            with open(self.currentDirectory + "\\" + file1, 'r') as f1:
+                with open(self.currentDirectory + "\\" + file2, 'r') as f2:
+                    for line in f1:
+                        catFile.write(line)
+                    for line in f2:
+                        catFile.write(line)
+            for line in catFile:
+                print(line)
 
-
+    def openFile(self, file):
+        self.currentDirectory += "\\"
+        self.currentDirectory += file
+        with open(self.currentDirectory, 'r') as f:
+            for line in f:
+                print(line)
 
 
 
@@ -34,24 +53,24 @@ class CommandPrompt(Actions):
 
     def runCommand(self):
         while True:
-            command = input("What should I do? Type 'ls' to see my content, 'cd <somewhere>' to go somewhere  or 'exit' to...exit!: ")
+            command = input("What should I do? Type 'ls /W' to see my content horizontally, 'ls /D' to see my content vertically, 'cd <somewhere>' to go somewhere  or 'exit' to...exit!: ")
 
+            request = command.split()
 
+            if request[0] == "ls":
+                self.ls(request)
 
-            if command == "ls":
-                self.ls()
+            if request[0] == "cd":
+                self.cd(request)
 
-            goToLocation = command.split()
-            if goToLocation[0] == "cd":
-                self.cd(goToLocation)
+            if request[0] == "cat":
+                self.cat(request[1], request[2])
 
-
-
+            if request[0] == "openFile":
+                self.openFile(request[1])
 
             if command == "exit":
                 break
-
-
 
 
 def main():
