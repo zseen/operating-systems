6230d5dae57aa1aef0b2f59e6c0e982afb0c1791
zseen <43151120+zseen@users.noreply.github.com>
2018-10-10 13:33:26 +0100

    Run "ls" and "cd" from class Actions

--- ListFolders.py
+++ ListFolders.py
@@ -1,23 +1,36 @@
 import os
 
-class CommandPrompt:
+
+class Actions:
     def __init__(self):
         self.currentDirectory = "C:\\Users"
         print(self.currentDirectory)
+
+    def ls(self):
+        print("")
+        print("The content of " + self.currentDirectory + " is: ")
+        for item in os.listdir(self.currentDirectory):
+            print(item)
+        print("")
+
+    def cd(self, instruction):
+        self.currentDirectory += "\\"
+        self.currentDirectory += ' '.join(instruction[1:])
+        print("You are in " + self.currentDirectory + " now")
+
+
+class CommandPrompt(Actions):
 
     def runCommand(self):
         while True:
             command = input("What should I do? Type 'ls' to see my content, 'cd <somewhere>' to go somewhere  or 'exit' to...exit!: ")
 
             if command == "ls":
-                print(os.listdir(self.currentDirectory))
+                self.ls()
 
             goToLocation = command.split()
             if goToLocation[0] == "cd":
-                self.currentDirectory += "\\"
-                self.currentDirectory += ' '.join(goToLocation[1:])
-                print("You are in " + self.currentDirectory+ " now")
-
+                self.cd(goToLocation)
 
 
 
