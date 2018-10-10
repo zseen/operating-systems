7940346bf1bcca8367c6e47ec61578bd78c433a2
zseen <43151120+zseen@users.noreply.github.com>
2018-10-10 11:42:17 +0100

    List the content of the current folder (C:\Users) if input is 'ls' and exit if 'exit'

--- ListFolders.py
+++ ListFolders.py
@@ -1,4 +1,25 @@
-buttonPressed = input("Please press 'k' to see my content: ")
+import os
 
-if buttonPressed == 'k':
-    
+class CommandPrompt:
+    def __init__(self):
+        self.currentDirectory = "C:\\Users"
+
+    def runCommand(self):
+        while True:
+            command = input("What should I do? Type 'ls' to see my content or 'exit' to...exit!: ")
+
+            if command == "ls":
+                print(os.listdir("C:\\Users"))
+
+            if command == "exit":
+                break
+
+
+
+
+def main():
+    command = CommandPrompt()
+    command.runCommand()
+
+if __name__ == "__main__":
+    main()
