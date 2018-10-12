3252c2093a605770066a930c6991eeaa4654e972
zseen <43151120+zseen@users.noreply.github.com>
2018-10-12 21:04:41 +0100

    Go to a folder if 'cd <somewhere>' is typed

--- ListFolders.py
+++ ListFolders.py
@@ -3,13 +3,23 @@ import os
 class CommandPrompt:
     def __init__(self):
         self.currentDirectory = "C:\\Users"
+        print(self.currentDirectory)
 
     def runCommand(self):
         while True:
-            command = input("What should I do? Type 'ls' to see my content or 'exit' to...exit!: ")
+            command = input("What should I do? Type 'ls' to see my content, 'cd <somewhere>' to go somewhere  or 'exit' to...exit!: ")
 
             if command == "ls":
-                print(os.listdir("C:\\Users"))
+                print(os.listdir(self.currentDirectory))
+
+            goToLocation = command.split()
+            if goToLocation[0] == "cd":
+                self.currentDirectory += "\\"
+                self.currentDirectory += ' '.join(goToLocation[1:])
+                print("You are in " + self.currentDirectory+ " now")
+
+
+
 
             if command == "exit":
                 break
