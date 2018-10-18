21c3db3a95f584a004617eafb5a55e69b22c8874
zseen <43151120+zseen@users.noreply.github.com>
2018-10-18 17:11:54 +0100

    Start refactoring CommandPrompt and CommandLineExecutor:
    - Methods ls() and cd() work

--- CommandPrompt/CommandExecutor.py
+++ CommandPrompt/CommandExecutor.py
@@ -1,103 +1,51 @@
 import os
 from itertools import islice
 import FilesystemHelpers as FH
-from pathlib import Path
 
-INITIAL_HELP = ("Options:" '\n'
-                "'ls' to see my content, " '\n'
-                "'ls -r' to see my content reversed, "  '\n'
-                "'cat <file>' to see the content of a text file, " '\n'
-                "'printTogether <file1> <file2>' to print two text files together," '\n'
-                "'joinTogether <file1> <file2> <file3>' merge file1 and file2 into file3," '\n'
-                "'mkdir <name>' to create a folder, " '\n'
-                "'rm <name>' to delete a file, " '\n'
-                "'head <file> <x>' to see the first x lines of a file, " '\n'
-                "'cd <somewhere>' to go somewhere, " '\n'
-                "'cd..' to go back to the previous level directory, or " '\n'
-                "'exit' to...exit! " '\n')
-
-
-class CommandLineExecutor:
+class CommandExecutor:
     def __init__(self):
         self.currentDirectory = FH.getHomeDirectory()
 
-    def run(self):
-        print(INITIAL_HELP)
-
-        while True:
-            print(self.currentDirectory)
-            command = input("What should I do?: ")
-
-            request = command.split()
-
-            if request[0] == "ls":
-                self.ls(request)
-
-            if request[0] == "cd":
-                self.cd(request[1:])
-
-            if request[0] == "joinTogether" and len(request) == 4:
-                self.joinTogether(request[1], request[2], request[3])
-
-            if request[0] == "printTogether" and len(request) == 3:
-                self.printTogether(request[1], request[2])
-
-            if request[0] == "cat" and len(request) == 2:
-                self.cat(request[1])
-
-            if request[0] == "mkdir" and len(request) == 2:
-                self.mkdir(request[1])
-
-            if request[0] == "rm" and len(request) == 2:
-                self.rm(request[1])
-
-            if request[0] == "head" and len(request) == 3:
-                self.head(request[1], request[2])
-
-            if command == "exit":
-                exit(0)
+    def getCurrentDirectory(self):
+        return self.currentDirectory
 
     def ls(self, instruction):
-        print("")
-        print("The content of " + self.currentDirectory + " is: ")
+        foldersList = []
 
         if len(instruction) == 2 and instruction[1] == "-r":
             for item in reversed(list(os.listdir(self.currentDirectory))):
-                print(item)
+                foldersList.append(item)
         else:
             for item in os.listdir(self.currentDirectory):
-                print(item)
-        print("")
+                foldersList.append(item)
+
+        return foldersList
 
     def cd(self,location):
         if location[0] == "..":
             currentDir = self.goBackOneLevel()
-            print("You are in " + currentDir + " now.")
             self.currentDirectory = currentDir
-
         else:
             currentDir = self.currentDirectory
             newPath = FH.createDirectoryPath(currentDir,''.join(location))
             exists = FH.checkIfDirectoryExists(newPath)
-            print("")
 
             if exists:
                 self.currentDirectory = newPath
-                print("You are in " + self.currentDirectory + " now.")
             else:
-                print("Directory does not exist")
+                return False
+        return self.currentDirectory
 
 
     def goBackOneLevel(self):
         atRootLevel = FH.isRootLevel(self.currentDirectory)
 
-        if atRootLevel:
-            print("You cannot go up, as you are in your home folder.")
-        else:
+        if not atRootLevel:
             pathToModify = list(os.path.split(self.currentDirectory))
             pathToModify.pop()
             for directory in pathToModify:
                 self.currentDirectory = str(directory)
+
         return self.currentDirectory
 
     def joinTogether(self, file1, file2, file3):
