1428f7a5f0c3dddac9037d68d3f41d5eb64f0127
zseen <43151120+zseen@users.noreply.github.com>
2018-10-19 23:13:12 +0100

    Start implementing CLE with GUI

--- GUI/FoldersGUI.py
+++ GUI/FoldersGUI.py
@@ -1,15 +1,24 @@
 from tkinter.tix import *
+from tkinter import filedialog
 from pathlib import Path
+import CommandExecutor as CLE
 
 class FolderPrinter:
     def __init__(self):
-        self.location = str(Path.home())
+        self.commandExecutor = CLE.CommandExecutor()
+        self.location = self.commandExecutor.getCurrentDirectory()
 
-    def getDirectories(self, location):
-        return os.listdir(location)
+    def printFileNameOnButton(self, name):
+        print(name)
+
+    def clickButtonSeeContent(self, button):
+        filename = self.commandExecutor.changeDirectory(str(button))
+
+        button.bind("<Mouse-1>", filename)
+        button.grid()
 
     def contentToButtons(self):
-        folders = self.getDirectories(self.location)
+        folders = self.commandExecutor.listDirectoryContent(self.location)
         root = Tk()
         frame = Frame(width=500, height=500)
         frame.grid()
@@ -19,12 +28,26 @@ class FolderPrinter:
 
         for folder in folders:
             f = Button(win, text=folder, fg="gray10", bg="old lace", command=lambda folderName=folder: self.printFileNameOnButton(folderName))
-            f.grid(sticky=N+S+E+W)
+            f.bind("<Mouse-1>", self.commandExecutor.changeDirectory(str(f)))
+
+
+            f.grid(sticky=N + S + E + W)
+            #self.clickButtonSeeContent(f)
+
+
+
+        #root = Tk()
+        #root.filename = filedialog.askopenfilename(initialdir=self.commandExecutor.getCurrentDirectory(), title="Select file",
+        #                                               filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*")))
+        #print(root.filename)
+
+
 
         root.mainloop()
 
-    def printFileNameOnButton(self, name):
-        print(name)
+
+
+
 
 
 def main():
