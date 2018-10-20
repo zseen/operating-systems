e437c392e09f81efd8e9443b4eb2eca4ef22d314
zseen <43151120+zseen@users.noreply.github.com>
2018-10-20 22:06:47 +0100

    Refactor - use better names

--- GUI/FoldersGUI.py
+++ GUI/FoldersGUI.py
@@ -1,35 +1,33 @@
 from tkinter.tix import *
-from tkinter import filedialog
-from pathlib import Path
 import CommandExecutor as CLE
 
 class FolderPrinter:
     def __init__(self):
-        self.commandExecutor = CLE.CommandExecutor()
+        self._commandExecutor = CLE.CommandExecutor()
         self._win = None
-        self.buttons = []
+        self._buttons = []
 
     def onClicked(self, name):
-        self.commandExecutor.changeDirectory(name)
+        self._commandExecutor.changeDirectory(name)
         self.renderButtons()
 
     def renderButtons(self):
-        for button in self.buttons:
+        for button in self._buttons:
             button.grid_remove()
-        folders = self.commandExecutor.listDirectoryContent(self.commandExecutor.getCurrentDirectory())
+        folders = self._commandExecutor.listDirectoryContent(self._commandExecutor.getCurrentDirectory())
         for folder in folders:
-            b = Button(self.win, text=folder, fg="gray10", bg="old lace", command=lambda folderName=folder: self.onClicked(folderName))
-            b.bind("<Mouse-1>", self.commandExecutor.changeDirectory(str(b)))
-            self.buttons.append(b)
+            b = Button(self._win, text=folder, fg="gray10", bg="old lace", command=lambda folderName=folder: self.onClicked(folderName))
+            b.bind("<Mouse-1>", self._commandExecutor.changeDirectory(str(b)))
+            self._buttons.append(b)
             b.grid(sticky=N + S + E + W)
 
-    def GUImainloop(self):
+    def mainLoopGUI(self):
         root = Tk()
         frame = Frame(width=500, height=500)
         frame.grid()
         swin = ScrolledWindow(frame, width=600, height=600)
         swin.grid()
-        self.win = swin.window
+        self._win = swin.window
 
         self.renderButtons()
 
@@ -38,7 +36,7 @@ class FolderPrinter:
 
 def main():
     c = FolderPrinter()
-    c.GUImainloop()
+    c.mainLoopGUI()
 
 if __name__ == "__main__":
     main()
