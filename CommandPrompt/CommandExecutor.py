05f7276a23bc04d30d5685387fe55c1e5ff1b2dc
zseen <43151120+zseen@users.noreply.github.com>
2018-10-20 15:52:43 +0100

    Refactor - methods return earlier

--- CommandPrompt/CommandExecutor.py
+++ CommandPrompt/CommandExecutor.py
@@ -26,21 +26,23 @@ class CommandExecutor:
         newPath = FH.createDirectoryPath(currentDir,''.join(location))
         exists = FH.checkIfDirectoryExists(newPath)
 
-        if exists:
-            self.currentDirectory = newPath
-            return True
-        return False
+        if not exists:
+            return False
+
+        self.currentDirectory = newPath
+        return True
 
     def goBackOneLevel(self):
         atRootLevel = FH.isRootLevel(self.currentDirectory)
 
-        if not atRootLevel:
-            pathToModify = list(os.path.split(self.currentDirectory))
-            pathToModify.pop()
-            for directory in pathToModify:
-                self.currentDirectory = str(directory)
-            return True
-        return False
+        if atRootLevel:
+            return False
+
+        pathToModify = list(os.path.split(self.currentDirectory))
+        pathToModify.pop()
+        for directory in pathToModify:
+            self.currentDirectory = str(directory)
+        return True
 
     def joinTwoFilesTogetherInThird(self, file1, file2, file3):
         path1 = FH.createDirectoryPath(self.currentDirectory, file1)
@@ -48,34 +50,35 @@ class CommandExecutor:
         path3 = FH.createDirectoryPath(self.currentDirectory, file3)
         content = []
 
-        if FH.checkIfFileExists(path1) and FH.checkIfFileExists(path2):
-            with open(path3, "w") as catFile, open(path1, 'r') as f1, open(path2, 'r') as f2:
-                for line in f1:
-                    catFile.write(line)
-                for line in f2:
-                    catFile.write(line)
-
-            with open(path3, "r") as catFile:
-                for line in catFile:
-                    content.append(line)
-            return content
-        else:
+        if not (FH.checkIfFileExists(path1) and FH.checkIfFileExists(path2)):
             return None
+
+        with open(path3, "w") as catFile, open(path1, 'r') as f1, open(path2, 'r') as f2:
+            for line in f1:
+                catFile.write(line)
+            for line in f2:
+                catFile.write(line)
+
+        with open(path3, "r") as catFile:
+            for line in catFile:
+                content.append(line)
+        return content
+
 
     def printTwoFilesTogether(self, file1, file2):
         path1 = FH.createDirectoryPath(self.currentDirectory, file1)
         path2 = FH.createDirectoryPath(self.currentDirectory, file2)
         content = []
 
-        if FH.checkIfFileExists(path1) and FH.checkIfFileExists(path2):
-            with open(path1, "r") as f1, open(path2, "r") as f2:
-                for line in f1:
-                    content.append(line)
-                for line in f2:
-                    content.append(line)
-            return content
-        else:
+        if not (FH.checkIfFileExists(path1) and FH.checkIfFileExists(path2)):
             return None
+
+        with open(path1, "r") as f1, open(path2, "r") as f2:
+            for line in f1:
+                content.append(line)
+            for line in f2:
+                content.append(line)
+        return content
 
 
     def getFileContent(self, file):
@@ -83,40 +86,42 @@ class CommandExecutor:
         exists = FH.checkIfFileExists(pathToFile)
         content = []
 
-        if exists:
-            with open(pathToFile, 'r') as f:
-                for line in f:
-                    content.append(line)
-            return content
-        else:
+        if not exists:
             return None
+
+        with open(pathToFile, 'r') as f:
+            for line in f:
+                content.append(line)
+        return content
 
     def makeDirectory(self, name):
         newPath = FH.createDirectoryPath(self.currentDirectory, name)
 
-        if not FH.checkIfDirectoryExists(newPath):
-            os.makedirs(newPath)
-            return True
-        return False
+        if FH.checkIfDirectoryExists(newPath):
+            return False
+
+        os.makedirs(newPath)
+        return True
 
     def removeFile(self, name):
         filePath = FH.createDirectoryPath(self.currentDirectory, name)
         exists = FH.checkIfFileExists(filePath)
-        if exists:
-            os.remove(filePath)
-            return True
-        else:
+        if not exists:
             return False
+
+        os.remove(filePath)
+        return True
 
     def getTextLinesFromBeginning(self, file, linesNum):
         path = FH.createDirectoryPath(self.currentDirectory, file)
         exists = FH.checkIfFileExists(path)
         content = []
-        if exists:
-            with open(path, "r") as f:
-                for line in islice(f, int(linesNum)):
-                    content.append(line)
-            return content
-        else:
+
+        if not exists:
             return None
+
+        with open(path, "r") as f:
+            for line in islice(f, int(linesNum)):
+                content.append(line)
+        return content
 
